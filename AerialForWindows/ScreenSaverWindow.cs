2ffb653c1a1ccd88fe59d1f338426245a6fcbe0a
Thomas Freudenberg <info@thomasfreudenberg.com>
2015-11-07 00:50:35 +0100

    added hyperlink to update message to go to settings directly

--- AerialForWindows/ScreenSaverWindow.cs
+++ AerialForWindows/ScreenSaverWindow.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
+using System.Windows.Threading;
 using AerialForWindows.Updates;
 
 namespace AerialForWindows {
     public class ScreenSaverWindow : Window {
         private readonly string _movieUrl;
         private TextBlock _textBlockError;
+        private TextBlock _textBlockUpdate;
 
         public ScreenSaverWindow(string movieUrl = null) {
             _movieUrl = movieUrl;
@@ -31,13 +35,13 @@ namespace AerialForWindows {
             }
             Content = grid;
 
-            UpdateManager.Instance.UpdatesAvailable += InstanceOnUpdatesAvailable;
+            UpdateManager.Instance.UpdatesAvailable +=
+                (_, __) => Dispatcher.BeginInvoke(DispatcherPriority.DataBind, (Action) OnUpdatesAvailable);
         }
 
-        private void InstanceOnUpdatesAvailable(object sender, EventArgs eventArgs) {
+        private void OnUpdatesAvailable() {
             var release = UpdateManager.Instance.Releases.OrderByDescending(r => r.Version).First();
-            _textBlockError = new TextBlock {
-                Text = $"Version {release.Version} is available. Open settings to update.",
+            var textBlockUpdate = new TextBlock {
                 Foreground = Brushes.White,
                 FontSize = 14,
                 Margin = new Thickness(2),
@@ -50,7 +54,14 @@ namespace AerialForWindows {
                     BlurRadius = 8
                 }
             };
-            ((Grid) Content).Children.Add(_textBlockError);
+            textBlockUpdate.Inlines.Add(new Run($"Version {release.Version} is available. "));
+            var hyperlink = new Hyperlink(new Run("Open settings to update.")) {
+                Command = new DelegateCommand(() => {
+                    Process.Start("control.exe", "desk.cpl,,@screensaver");
+                })
+            };
+            textBlockUpdate.Inlines.Add(hyperlink);
+            ((Grid) Content).Children.Add(textBlockUpdate);
         }
 
         private void MediaElementOnMediaFailed(object sender, ExceptionRoutedEventArgs args) {
