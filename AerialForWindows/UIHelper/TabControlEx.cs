98a516f37fe530fab2ab0324336938fe703acaea
Thomas Freudenberg <info@thomasfreudenberg.com>
2016-07-02 18:28:35 +0200

    using tabs in settings view

---
+++ AerialForWindows/UIHelper/TabControlEx.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AerialForWindows.UIHelper {
+    public class TabControlEx : TabControl {
+        public override void OnApplyTemplate() {
+            base.OnApplyTemplate();
+
+            double maxHeight = 0;
+            double maxWidth = 0;
+            foreach (TabItem item in Items) {
+                var content = item.Content as FrameworkElement;
+                if (content == null) continue;
+
+                if (!content.IsMeasureValid)
+                    content.Measure(new Size(int.MaxValue, int.MaxValue));
+
+                var height = content.DesiredSize.Height;
+                if (maxHeight < height)
+                    maxHeight = height;
+                var width = content.DesiredSize.Height;
+                if (maxWidth < width)
+                    maxWidth = width;
+            }
+
+            var contentHost = GetTemplateChild("PART_SelectedContentHost") as FrameworkElement;
+            Debug.Assert(contentHost != null);
+            contentHost.MinHeight = maxHeight;
+            contentHost.MinWidth = maxWidth;
+        }
+    }
+}
