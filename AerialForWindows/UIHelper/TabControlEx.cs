5dd27be01d475799f5176e1e12f1fb01a858f60a
Thomas Freudenberg <info@thomasfreudenberg.com>
2016-10-23 19:01:05 +0200

    improved sizing of settings view

--- AerialForWindows/UIHelper/TabControlEx.cs
+++ AerialForWindows/UIHelper/TabControlEx.cs
@@ -1,33 +1,66 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace AerialForWindows.UIHelper {
     public class TabControlEx : TabControl {
         public override void OnApplyTemplate() {
             base.OnApplyTemplate();
 
-            double maxHeight = 0;
-            double maxWidth = 0;
-            foreach (TabItem item in Items) {
-                var content = item.Content as FrameworkElement;
-                if (content == null) continue;
-
-                if (!content.IsMeasureValid)
-                    content.Measure(new Size(int.MaxValue, int.MaxValue));
-
-                var height = content.DesiredSize.Height;
-                if (maxHeight < height)
-                    maxHeight = height;
-                var width = content.DesiredSize.Height;
-                if (maxWidth < width)
-                    maxWidth = width;
-            }
-
             var contentHost = GetTemplateChild("PART_SelectedContentHost") as FrameworkElement;
             Debug.Assert(contentHost != null);
-            contentHost.MinHeight = maxHeight;
-            contentHost.MinWidth = maxWidth;
+
+            contentHost.SetBinding(MinWidthProperty, new Binding {
+                Path = new PropertyPath("Items"),
+                Source = this,
+                Converter = new MaxDimensionConverter(false)
+            });
+            contentHost.SetBinding(MinHeightProperty, new Binding {
+                Path = new PropertyPath("Items"),
+                Source = this,
+                Converter = new MaxDimensionConverter(true)
+            });
+        }
+
+        private class MaxDimensionConverter : IValueConverter {
+            private readonly bool _checkHeight;
+
+            public MaxDimensionConverter(bool checkHeight) {
+                _checkHeight = checkHeight;
+            }
+
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+                var items = value as ItemCollection;
+                if (items == null) {
+                    return null;
+                }
+
+                double max = 0;
+                foreach (TabItem item in items) {
+                    var content = item.Content as FrameworkElement;
+                    if (content == null) {
+                        continue;
+                    }
+
+                    if (!content.IsMeasureValid) {
+                        content.Measure(new Size(int.MaxValue, int.MaxValue));
+                    }
+
+                    var length = _checkHeight ? content.DesiredSize.Height : content.DesiredSize.Width;
+                    if (max < length) {
+                        max = length;
+                    }
+                }
+
+                return max;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+                return DependencyProperty.UnsetValue;
+            }
         }
     }
 }
